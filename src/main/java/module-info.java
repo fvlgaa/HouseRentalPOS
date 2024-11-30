7d860db9804515cdb505d88147108007ea91571f
alabi leke <w0838407@myscc.ca>
2024-11-30 00:42:29 -0500

    i fixed the build granle and made the sure the main was working

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
-module com.example.houserentalpos {
+module com.example.houserentals {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
 
 
-    opens com.example.houserentalpos to javafx.fxml;
-    exports com.example.houserentalpos;
+    opens com.example.houserentals to javafx.fxml;
+    exports com.example.houserentals;
 }
